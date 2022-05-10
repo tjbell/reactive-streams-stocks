5f290e0e51a21e3a1575c3db5103e51b024151b0
Tom Bell <tjbell@gmail.com>
2022-05-10 14:35:36 -0600

    Example Tests

--- src/test/java/com/pluralsight/springwebflux5/stocktrading/controller/StockTradingApplicationTests.java
+++ src/test/java/com/pluralsight/springwebflux5/stocktrading/controller/StockTradingApplicationTests.java
@@ -2,46 +2,32 @@ package com.pluralsight.springwebflux5.stocktrading.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.io.IOException;
-import java.lang.IllegalAccessException;
+
 import static org.junit.jupiter.api.Assertions.*;
 import java.lang.reflect.*;
-import java.lang.NoSuchMethodException;
-import java.lang.ClassNotFoundException;
-import java.lang.SecurityException;
 import java.util.*;
 
 @SpringBootTest
 class StockTradingApplicationTests {
 
     @Test
-    void constructorTest() throws IllegalAccessException, IOException {
-        Field[] fields = Stock.class.getDeclaredFields();
-        assertEquals(1, fields.length, "==> Have you created a `private String` field in the `Stock` class.");
-        fields[0].setAccessible(true);
-    }
-    
-    // @Test 
-    // void getOneStockTest() throws IllegalAccessException, IOException, ClassNotFoundException, NoSuchMethodException, SecurityException {
-    //     Class stocksControllerClass = Class.forName("com.pluralsight.springwebflux5.stocktrading.controller.StocksController");
-    //     Method[] methods = stocksControllerClass.getDeclaredMethods();
-    //     System.out.println(Arrays.stream(methods).filter(method -> method.getName() == "getOneStock").toString());
-    //     // assertNotNull(methods, "==> Have you created a `method` called `getOneStock`?");
-    // }
-
-    @Test
-    void getOneStockTest() {
-        int actual = 0;
-        Method getOneStockMethod = null;
-    
+    void getDeclaredMethodsTest() {
+        boolean getOneStockMethod = false;
         try {
-            Class stocksControllerClass = Class.forName("com.pluralsight.springwebflux5.stocktrading.controller.StocksController");
-            getOneStockMethod = stocksControllerClass.getMethod("getOneStock");
-            // actual = (int) getOneStockMethod.invoke(statistics);
+            getOneStockMethod = Arrays.stream(StocksController.class.getDeclaredMethods()).anyMatch(method -> method.getName() == "getOneStock");
         } catch (Throwable e) { }
 
-        assertNotNull(getOneStockMethod, "==> Have you defined the getOneStock method?");
-        // assertEquals(185, actual, "==> The word count doesn't seem to be correct. Are you splitting on ` ` (space).");
+        assertTrue(getOneStockMethod, "Do you have a `getOneStock` method in the `StocksController` class?");
     }
 
+    @Test
+    void getDeclaredMethodTest() {
+        Method getOneStockMethod = null;
+        try {
+            getOneStockMethod = StocksController.class.getDeclaredMethod("getOneStock", new Class[] { String.class });
+        } catch (Throwable e) { }
+        assertNotNull(getOneStockMethod, "Do you have a `getOneStock` method in the `StocksController` class?");
+        assertEquals(getOneStockMethod.getReturnType().getName(), "reactor.core.publisher.Mono", "Does the `getOneStock` method have the correct return type?");
+        // You could check the parameter count and types with getParameterCount and getParameterTypes
+    }
 }
